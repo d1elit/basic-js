e9e1bb3fd1dbf2990f3c9a864c4a539ae66e68b8
Artem Gromiko <dropdox12@gmail.com>
2022-10-17 16:05:28 +0300

    feat: add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample( sampleActivity ) {
+    let result
+    if (typeof(sampleActivity) !== 'string') {
+        return false;
+    }   
+    isNaN(+sampleActivity)? false :
+    result = Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD))
+    return  result < 0 ||  isNaN(result) || result == Infinity  || result == -Infinity  ? false : result   
 }
 
 module.exports = {
